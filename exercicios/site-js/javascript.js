deccfdf192b72705aee5f3cf483a3e9c87a5bda8
MTbustamante98 <139372893+MTbustamante98@users.noreply.github.com>
2023-11-04 14:26:27 -0300

    update

--- exercicios/site-js/javascript.js
+++ exercicios/site-js/javascript.js
@@ -3,17 +3,22 @@ function carregar() {
     var img = document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 11
+    var hora = 4
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora <= 12) {
+    if (hora >= 6 && hora <= 12) {
         img.src = 'manha.jpg'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 13 && hora <= 18 ) {
         img.src = 'tarde.jpg'
         document.body.style.background = '#00AAF5'
-    } else {
+    } else if (hora >= 19 && hora <= 23) {
         img.src = 'noite.jpg'
         document.body.style.background = '#530af4'
+    } else {
+        img.src = 'madrugada.jpg'
+        document.body.style.background = 'black'
+        msg.innerHTML = `Agora é ${hora}hr da madrugada` 
     }
+
 
 }
